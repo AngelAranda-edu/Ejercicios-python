c560d596f0211629ac3569b371749543d9ff2e4a
Ángel Luis <aarames2208@g.educaand.es>
2025-10-07 13:11:47 +0200

    Ejercicio 2

---
+++ ejercicio2.py
@@ -0,0 +1,3 @@
+b = float(input("Introduce la base del rectangulo\n"))
+a = float(input("Introduce la altura rectangulo\n"))
+print("Perimetro del rectangulo = "+ str(2*(b+a)) + "\nÁrea del rectangulo = " + str(b*a))
