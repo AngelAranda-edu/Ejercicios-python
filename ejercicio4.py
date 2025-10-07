b7ca80634912576dabb2875f83983ad16328ebdf
angel-aranda <aarames2208@g.educaand.es>
2025-10-08 00:46:01 +0200

    Ejercicio 5

--- ejercicio4.py
+++
@@ -1,9 +0,0 @@
-numero1 = 3
-numero2 = 5
-print(f"""{"*"* 50}\nSuma de los dos números : {int(numero1) + int(numero2)}
-Resta de los dos números : {int(numero1) - int(numero2)}
-Multiplicación de los dos números : {int(numero1) * int(numero2)}
-División de los dos números : {int(numero1) / int(numero2)}
-Multiplicación de los dos números : {int(numero1) * int(numero2)}
-{"*"* 50}
-""")
---
+++ ejercicio5.py
@@ -0,0 +1,2 @@
+gf = 5 # Grados Fahrenheit
+print(f"Grados Celsius: {(gf-32)*5/9}")
