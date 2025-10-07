c9158a82e4fa493f2a4559a981f0f325dc98e4c6
angel-aranda <aarames2208@g.educaand.es>
2025-10-08 00:35:04 +0200

    Ejercicio 4

---
+++ ejercicio4.py
@@ -0,0 +1,9 @@
+numero1 = 3
+numero2 = 5
+print(f"""{"*"* 50}\nSuma de los dos números : {int(numero1) + int(numero2)}
+Resta de los dos números : {int(numero1) - int(numero2)}
+Multiplicación de los dos números : {int(numero1) * int(numero2)}
+División de los dos números : {int(numero1) / int(numero2)}
+Multiplicación de los dos números : {int(numero1) * int(numero2)}
+{"*"* 50}
+""")
