d8c37e2c5672db576b5f6b5ca8425a57ae642ea3
angel-aranda <aarames2208@g.educaand.es>
2025-10-08 11:21:37 +0200

    Ejercicio 12

--- ejercicio11.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Ingrese el primer número: "))
-num2= int(input("Ingrese el segundo número: "))
-
-if num1 > num2:
-    print(f"La distancia entre {num1} y {num2} es {num1 - num2}")
-elif num2 > num1:
-    print(f"La distancia entre {num1} y {num2} es {num2 - num1}")
-elif num1 == num2:
-    print("Los números son iguales, la distancia es 0")
---
+++ ejercicio12.py
@@ -0,0 +1,7 @@
+x1 = int(input("Introduce x1: "))
+y1 = int(input("Introduce y1: "))
+x2 = int(input("Introduce x2: "))
+y2 = int(input("Introduce y2: "))
+
+distancia = ((x2 - x1)**2 + (y2 - y1)**2) ** 0.5
+print(f"La distancia entre los puntos es: {distancia}")
