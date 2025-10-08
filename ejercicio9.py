8d62c6d19ff06b717e94aa44f08c021831a4ad89
angel-aranda <aarames2208@g.educaand.es>
2025-10-08 10:07:12 +0200

    Ejercicio 9

---
+++ ejercicio9.py
@@ -0,0 +1,5 @@
+pproductos = [20,50,40]
+total = 0
+for a in pproductos:
+    total = total + a
+print(f"El precio total de la compra es: {total}\nEl precio total de la compra con descuento es: {total - total * 0.15}")
