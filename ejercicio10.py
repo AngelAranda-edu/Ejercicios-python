71c69357bdeeabee5c71e2dc64a6d5f000b81f82
angel-aranda <aarames2208@g.educaand.es>
2025-10-08 10:02:52 +0200

    Ejercicio 10

---
+++ ejercicio10.py
@@ -0,0 +1,9 @@
+parcial1 = 6
+parcial2 = 7
+parcial3 = 5
+promedio_parciales = (parcial1 + parcial2 + parcial3) / 3
+examen_final = 4.5
+trabajo_final = 8
+calificacion_final = (promedio_parciales * 0.55) + (examen_final * 0.30) + (trabajo_final * 0.15)
+
+print(f"La calificación final es: {round(calificacion_final, 2)}")
