e7863700381cca41fc5476d1734776f86d947f9a
angel-aranda <aarames2208@g.educaand.es>
2025-10-08 11:39:49 +0200

    Ejercicio 14

--- ejercicio13.py
+++
@@ -1,2 +0,0 @@
-numero = 64
-print(f"Raiz cuadrada de {numero} es {numero**(1/2)}\nRaiz cubica de {numero} es {numero**(1/3)}")
---
+++ ejercicio14.py
@@ -0,0 +1,2 @@
+numero = 64
+print(f"Número: {numero}\nNúmero invertido: {str(numero)[1]}{str(numero)[0]}")
