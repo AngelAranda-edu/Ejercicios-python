3a4d6d6ac29e1e9092647429c9e24d728cdb71a3
angel-aranda <aarames2208@g.educaand.es>
2025-10-08 11:31:26 +0200

    Ejercicio 13

--- ejercicio12.py
+++
@@ -1,7 +0,0 @@
-x1 = int(input("Introduce x1: "))
-y1 = int(input("Introduce y1: "))
-x2 = int(input("Introduce x2: "))
-y2 = int(input("Introduce y2: "))
-
-distancia = ((x2 - x1)**2 + (y2 - y1)**2) ** 0.5
-print(f"La distancia entre los puntos es: {distancia}")
---
+++ ejercicio13.py
@@ -0,0 +1,2 @@
+numero = 64
+print(f"Raiz cuadrada de {numero} es {numero**(1/2)}\nRaiz cubica de {numero} es {numero**(1/3)}")
