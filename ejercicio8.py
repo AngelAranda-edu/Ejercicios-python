78b7a312c3aaff19b94996a2255d30c4ba53035f
Angel Luis Aranda Mesa <143511422+Angel-Aranda@users.noreply.github.com>
2025-10-09 12:23:03 +0200

    Calculate total commissions from sales list

--- ejercicio8.py
+++ ejercicio8.py
@@ -5,3 +5,12 @@ v3 = 600 # venta 3
 comisiones= float(v1)*0.10 + float(v2)*0.10 + float(v3)*0.10
 
 print(f"Total de comisiones: {comisiones}\nTotal al mes: {float(comisiones) + float(sb)}")
+
+sueldo = 1000
+ventas =  [500,300,600]
+t_comisiones= 0
+
+for v in ventas:
+  t_comisiones += float(v) * 0.10
+t_comisiones = int(t_comisiones)
+print(f"Total comisiones {t_comisiones}\nTotal nomina {sueldo + t_comisiones}")
