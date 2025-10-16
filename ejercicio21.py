2050e966d79436d484d00e851b4fa297a196ae6b
Ángel Luis <aarames2208@g.educaand.es>
2025-10-16 12:08:08 +0200

    Ejercicio 39

--- ejercicio21.py
+++
@@ -1,6 +0,0 @@
-numero= int(input ("Introduce un número para calcular su factorial: "))
-factorial =  1
-while numero > 0:
-	factorial = factorial * numero
-	numero -= 1
-print(f"El factorial es {factorial}")
