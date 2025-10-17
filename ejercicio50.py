7b75d6a8da298310d757268d3a5fc18740110693
angel-aranda <aarames2208@g.educaand.es>
2025-10-17 08:57:00 +0200

    Ejercicio 53

--- ejercicio50.py
+++
@@ -1,7 +0,0 @@
-cadena = input("Introduce una cadena: ")
-cadena = cadena.lower()
-
-if cadena == cadena[::-1]:
-    print("La cadena es una palabra palíndroma")
-else:
-    print("La cadena no es una palabra palíndroma")
