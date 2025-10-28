a129f2b4a37ba5b3c6716c7ac48b1c09fa7f3a7d
Ángel Luis <aarames2208@g.educaand.es>
2025-10-28 12:33:34 +0100

    Ejercicio 76

--- ejercicio75.py
+++
@@ -1,4 +0,0 @@
-def EscribirCentrado (texto):
-	print(f"{int(40-len(texto)/2)*' '} {texto} {int(40-len(texto)/2)*' '}")
-	print(f"{int(40-len(texto)/2)*' '} {'='*len(texto)} {int(40-len(texto)/2)*' '}")
-EscribirCentrado("Hola")
